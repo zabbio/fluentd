f281c08df23cf8c990f30a31724d6771371a1c2d
zabbio <zabbio.zabbio@gmail.com>
2012-09-13 21:36:01 +0900

    コメント編集

--- install/td-agent-install.sh
+++ install/td-agent-install.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-# apache,fluent-plugin-s2が依存するパッケージをインストール
-yum -y install httpd.x86_64 \
-	libxml2-devel.x86_64 libxslt-devel.x86_64 gcc make
+# td-agent,plugin が依存するパッケージをインストール
+yum -y install httpd.x86_64 libxml2-devel.x86_64 libxslt-devel.x86_64 gcc make
 
-# td-agent 用のリポジトリを登録
+# td-agent のリポジトリを登録
 cat <<'EOF' > /etc/yum.repos.d/td.repo
 [treasuredata]
 name=TreasureData
@@ -20,11 +19,8 @@ yum --enablerepo=treasuredata -y install td-agent
 chgrp td-agent /var/log/httpd/
 chmod g+rx /var/log/httpd/
 
-# fluent-plugin-s3 をインストール
-#/usr/lib64/fluent/ruby/bin/fluent-gem install fluent-plugin-s3
-
 # td-agentの設定
-cp /etc/td-agent/td-agent.conf /etc/td-agent/td-agent.conf.org
+cp /etc/td-agent/td-agent.conf /etc/td-agent/td-agent.conf.bk
 
 cat <<_EOT_ 1>/etc/td-agent/td-agent.conf
 ####
@@ -50,16 +46,17 @@ cat <<_EOT_ 1>/etc/td-agent/td-agent.conf
    type s3
 
    aws_key_id [ここにaccess keyを入力]
-   aws_sec_key [ここにsecret access keyを入力]
+   aws_sec_key [ここにsecret keyを入力]
    s3_bucket [ここにバケット名を入力]
-   s3_endpoint ncss.nifty.com
+   s3_endpoint s3-ap-northeast-1.amazonaws.com
 
-# バケット上の下記のパスに保存されます。適宜修正してください。
+# バケットの下記パスに保存されます。
    path logs/
    buffer_path /var/log/td-agent/buffer/s3
 
    time_slice_format %Y%m%d-%H
    time_slice_wait 10m
+   utc
 </match>
 _EOT_
 
@@ -70,4 +67,4 @@ _EOT_
 service httpd start
 
 echo "==== インストール完了"
-echo "/etc/td-agent/td-agent.conf にニフティクラウドストレージアクセスキー/バケット名を設定後、td-agentを起動してください。"
+echo "/etc/td-agent/td-agent.conf にアクセスキー/バケット名を設定後、td-agentを起動してください。"
